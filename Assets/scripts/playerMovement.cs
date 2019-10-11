13c454ece220c6613a222a77212eb4fabee5d194
Angus <angusoneill972@gmail.com>
2019-10-11 23:07:58 +1100

    started work on basic character models and began physics stuff

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float dir = 150f;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, dir * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -dir * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, Time.deltaTime * -150, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, Time.deltaTime * 150, 0);
+        }
+    }
+}
+
+
+
+
+
